b908b1f07f7bb2d5641da720941e3132eb99e169
YuxinxinChen <yxxchen@ucdavis.edu>
2023-01-10 17:37:09 -0800

    miner

--- pybind_cuda_module/test_2gpu.py
+++ pybind_cuda_module/test_2gpu.py
@@ -4,14 +4,17 @@ import sys
 sys.path.append("./build/lib.linux-x86_64-cpython-310")
 import hello_cuda
 
-cuda0 = torch.device('cuda:0')
-cuda1 = torch.device('cuda:1')
+num_of_device = torch.cuda.device_count()
+print("number of devices: %d" % num_of_device)
+cudaDevice = []
+for i in range(num_of_device):
+    cudaDevice.append(torch.device(i))
 
 a = torch.tensor([1,2])
 b = torch.tensor([4,5])
 
-a_cuda = a.to(cuda0)
-b_cuda = b.to(cuda1)
+a_cuda = a.to(cudaDevice[0])
+b_cuda = b.to(cudaDevice[1])
 torch.cuda.synchronize()
 hello_cuda.hello(a_cuda)
 torch.cuda.synchronize()
